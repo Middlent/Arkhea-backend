93094d40ac74ea9c9987ba2768813ed907677124
Milton <milton_pinheiro@outlook.com>
2025-03-31 21:41:23 -0400

    Banco agr se cria sozinho

--- db.js
+++ db.js
@@ -1,7 +1,16 @@
 // Criação das tabelas
 
+async function create_database(client) {
+    const res = await client.query(`SELECT datname FROM pg_catalog.pg_database WHERE datname = 'Arkhea'`);
+
+    if (res.rowCount === 0) {
+        await client.query(`CREATE DATABASE "Arkhea";`);
+    }
+    return client.release();
+}
+
 async function create_product_table(client) {
-    await client.query('CREATE TABLE IF NOT EXISTS public."Produto"'+
+    return await client.query('CREATE TABLE IF NOT EXISTS public."Produto"'+
                         '('+
                         '    "cod" bigint NOT NULL,'+
                         '    "nome" text COLLATE pg_catalog."default" NOT NULL,'+
@@ -13,7 +22,7 @@ async function create_product_table(client) {
 }
 
 async function create_sale_table(client) {
-    await client.query('CREATE SEQUENCE IF NOT EXISTS public.Vendas_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;')
+    return await client.query('CREATE SEQUENCE IF NOT EXISTS public.Vendas_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;')
     .then(
         () => {
             client.query('CREATE TABLE IF NOT EXISTS public."Venda"'+
@@ -28,7 +37,7 @@ async function create_sale_table(client) {
 }
 
 async function create_product_sale_table(client) {
-    await client.query('CREATE TABLE IF NOT EXISTS public."ProdutoVendido"'+
+    return await client.query('CREATE TABLE IF NOT EXISTS public."ProdutoVendido"'+
                         '('+
                         '    "produto" bigint NOT NULL,'+
                         '    "venda" integer NOT NULL DEFAULT 0,'+
@@ -54,23 +63,29 @@ async function connect() {
         return global.connection.connect();
 
     const { Pool } = require('pg');
+    const dbpool  = new Pool({
+        connectionString: process.env.DATABASE_STRING
+    });
+    const dbclient = await dbpool.connect();
+
+    await create_database(dbclient)
+    
     const pool  = new Pool({
         connectionString: process.env.CONNECTION_STRING
     });
-
     const client = await pool.connect();
     console.log('Criou a Pool de conexões');
 
     const res = await client.query('SELECT NOW()');
     console.log(res.rows[0]);
 
+    
     await create_product_table(client)
         .then(() => {
             create_sale_table(client).then( () => {
                 create_product_sale_table(client)
             })
         })
-
     client.release();
     global.connection = pool;
     return pool.connect();
